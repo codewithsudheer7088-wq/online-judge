46a089ce75e748b105d2cc866491ed796869270d
Sudhir <codewithsudheer7088@gmail.com>
2026-04-28 20:55:41 -0700

    fix local and render db ssl

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -1,15 +1,24 @@
 require("dotenv").config();
-
 const { Sequelize } = require("sequelize");
+
+const isRender = process.env.NODE_ENV === "production";
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
-  String(process.env.DB_PASSWORD), // 🔥 IMPORTANT
+  String(process.env.DB_PASSWORD),
   {
     host: process.env.DB_HOST,
     dialect: "postgres",
-    port: process.env.DB_PORT
+    port: process.env.DB_PORT,
+    dialectOptions: isRender
+      ? {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false
+          }
+        }
+      : {}
   }
 );
 
