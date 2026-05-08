f8ea2d6b69bcbdb7ec8d5a5b3b0b6a57b8f063f9
Sudhir <codewithsudheer7088@gmail.com>
2026-05-08 02:14:18 -0700

    Added seed problems route

--- Backend/src/routes/problem.route.js
+++ Backend/src/routes/problem.route.js
@@ -1,14 +1,78 @@
 const express = require("express");
 const router = express.Router();
 
+const Problem = require("../models/problem.model");
+
 const {
   createProblem,
   getProblems,
   getProblemById,
 } = require("../controllers/problem.controller");
 
+// CREATE
 router.post("/", createProblem);
-router.get("/", getProblems); // 🔥 filter yaha hai
+
+// GET ALL
+router.get("/", getProblems);
+
+// GET BY ID
 router.get("/:id", getProblemById);
+
+// 🔥 SEED PROBLEMS
+router.get("/seed/data", async (req, res) => {
+
+  await Problem.create({
+    title: "Add Two Numbers",
+    description: "Add two integers",
+    difficulty: "easy",
+
+    testCases: [
+      {
+        input: "2 3",
+        output: "5",
+      },
+      {
+        input: "10 20",
+        output: "30",
+      },
+    ],
+  });
+
+  await Problem.create({
+    title: "Multiply Two Numbers",
+    description: "Multiply two integers",
+    difficulty: "medium",
+
+    testCases: [
+      {
+        input: "2 3",
+        output: "6",
+      },
+      {
+        input: "10 20",
+        output: "200",
+      },
+    ],
+  });
+
+  await Problem.create({
+    title: "Factorial",
+    description: "Find factorial",
+    difficulty: "hard",
+
+    testCases: [
+      {
+        input: "5",
+        output: "120",
+      },
+      {
+        input: "3",
+        output: "6",
+      },
+    ],
+  });
+
+  res.send("Problems Added");
+});
 
 module.exports = router;
