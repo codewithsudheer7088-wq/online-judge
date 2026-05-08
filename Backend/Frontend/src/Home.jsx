71f175b4ed6c58cabeeb3c71e49644f2aee9fac0
Sudhir <codewithsudheer7088@gmail.com>
2026-05-08 01:56:42 -0700

    Fixed difficulty filter values

--- Backend/Frontend/src/Home.jsx
+++ Backend/Frontend/src/Home.jsx
@@ -4,45 +4,84 @@ import { Link } from "react-router-dom";
 import "./App.css";
 
 export default function Home() {
+
   const [problems, setProblems] = useState([]);
   const [filter, setFilter] = useState("");
 
   useEffect(() => {
+
     axios
       .get(
         `https://online-judge-xvbw.onrender.com/api/problems${
           filter ? `?difficulty=${filter}` : ""
         }`
       )
-      .then((res) => setProblems(res.data));
+      .then((res) => setProblems(res.data))
+
+      .catch((err) => {
+        console.log(err);
+      });
+
   }, [filter]);
 
   return (
+
     <div className="container">
+
       <h1>⚡ Problems</h1>
 
-      <select onChange={(e) => setFilter(e.target.value)}>
-        <option value="">All</option>
-        <option value="easy">Easy</option>
-        <option value="medium">Medium</option>
-        <option value="hard">Hard</option>
+      <select
+        onChange={(e) =>
+          setFilter(e.target.value)
+        }
+      >
+
+        <option value="">
+          All
+        </option>
+
+        <option value="Easy">
+          Easy
+        </option>
+
+        <option value="Medium">
+          Medium
+        </option>
+
+        <option value="Hard">
+          Hard
+        </option>
+
       </select>
 
       {problems.map((p) => (
+
         <div className="card" key={p.id}>
+
           <h2>{p.title}</h2>
 
-          <span className={`badge ${p.difficulty}`}>
+          <span
+            className={`badge ${p.difficulty}`}
+          >
             {p.difficulty}
           </span>
 
           <Link to={`/problem/${p.id}`}>
-            <button style={{ marginTop: "10px" }}>
+
+            <button
+              style={{
+                marginTop: "10px",
+              }}
+            >
               Start Challenge 🚀
             </button>
+
           </Link>
+
         </div>
+
       ))}
+
     </div>
   );
 }
