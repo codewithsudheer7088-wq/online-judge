06e8ba4c44e683191bdc400dd261a150343e61cc
Sudhir <codewithsudheer7088@gmail.com>
2026-05-08 03:02:48 -0700

    Removed crasing seed route

--- Backend/src/routes/problem.route.js
+++ Backend/src/routes/problem.route.js
@@ -1,78 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
-const Problem = require("../models/problem.model");
-
 const {
   createProblem,
   getProblems,
   getProblemById,
 } = require("../controllers/problem.controller");
 
-// CREATE
+// CREATE PROBLEM
 router.post("/", createProblem);
 
-// GET ALL
+// GET ALL PROBLEMS
 router.get("/", getProblems);
 
-// 🔥 SEED PROBLEMS
-router.get("/seed/data", async (req, res) => {
-
-  await Problem.create({
-    title: "Add Two Numbers",
-    description: "Add two integers",
-    difficulty: "easy",
-
-    testCases: [
-      {
-        input: "2 3",
-        output: "5",
-      },
-      {
-        input: "10 20",
-        output: "30",
-      },
-    ],
-  });
-
-  await Problem.create({
-    title: "Multiply Two Numbers",
-    description: "Multiply two integers",
-    difficulty: "medium",
-
-    testCases: [
-      {
-        input: "2 3",
-        output: "6",
-      },
-      {
-        input: "10 20",
-        output: "200",
-      },
-    ],
-  });
-
-  await Problem.create({
-    title: "Factorial",
-    description: "Find factorial",
-    difficulty: "hard",
-
-    testCases: [
-      {
-        input: "5",
-        output: "120",
-      },
-      {
-        input: "3",
-        output: "6",
-      },
-    ],
-  });
-
-  res.send("Problems Added");
-});
-
-// GET BY ID
+// GET SINGLE PROBLEM
 router.get("/:id", getProblemById);
 
 module.exports = router;
