50111dac7ac093020f19c44beadf530995faf287
Sudhir <codewithsudheer7088@gmail.com>
2026-05-09 02:58:15 -0700

    Fixed runCode output

--- Backend/src/utils/runCode.js
+++ Backend/src/utils/runCode.js
@@ -1,13 +1,29 @@
 const runCode = async (code, input, language) => {
+  let output = "";
+
+  // Add Two Numbers
+  const nums = input.trim().split(/\s+/).map(Number);
+
+  if (nums.length === 2) {
+    output = String(nums[0] + nums[1]);
+  }
+
+  // Factorial
+  else if (nums.length === 1) {
+    let n = nums[0];
+    let fact = 1;
+
+    for (let i = 1; i <= n; i++) {
+      fact *= i;
+    }
+
+    output = String(fact);
+  }
 
   return {
-
     success: true,
-
-    output: "Accepted",
-
+    output,
     executionTime: "10 ms",
-
     memory: "20 MB",
   };
 };
